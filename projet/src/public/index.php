43e8c62e65beee326f3ee8705c3ec8381f7b2e5c
Jyeil <jyeil9@laposte.net>
2016-05-08 15:12:19 +0200

    modif de index.php pour inclure eloquent

--- projet/src/public/index.php
+++ projet/src/public/index.php
@@ -4,13 +4,26 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$config['displayErrorDetails'] = true;
-$config['db']['host']   = "localhost";
-$config['db']['user']   = "root";
-$config['db']['pass']   = "";
-$config['db']['dbname'] = "cmidb";
+$app = new \Slim\App;
 
-$app = new \Slim\App(["settings" => $config]);
+// Database information
+$settings = array(
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'cmidb',
+    'username' => 'root',
+    'password' => '',
+    'collation' => 'utf8_general_ci',
+    'prefix' => ''
+);
+
+$container = new Illuminate\Container\Container;
+$connFactory = new \Illuminate\Database\Connectors\ConnectionFactory($container);
+$conn = $connFactory->make($settings);
+$resolver = new \Illuminate\Database\ConnectionResolver();
+$resolver->addConnection('default', $conn);
+$resolver->setDefaultConnection('default');
+\Illuminate\Database\Eloquent\Model::setConnectionResolver($resolver);
 
 $app->run();
 
