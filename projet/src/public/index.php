35f9568765220a479c3159dbdfe35b95b3ff7602
Jyeil <jyeil9@laposte.net>
2016-05-08 15:01:05 +0200

    ajout du repo public

---
+++ projet/src/public/index.php
@@ -0,0 +1,17 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$config['displayErrorDetails'] = true;
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "root";
+$config['db']['pass']   = "";
+$config['db']['dbname'] = "cmidb";
+
+$app = new \Slim\App(["settings" => $config]);
+
+$app->run();
+
+?>
