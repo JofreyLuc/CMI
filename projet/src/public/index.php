e173c3337b1ae027df4917760ccaa96caac8672a
mulhauser <root@MacBook-Air-de-Remy.local>
2016-05-11 14:08:50 +0200

    routage

--- projet/src/public/index.php
+++ projet/src/public/index.php
@@ -6,25 +6,19 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
-// Database information
-$settings = array(
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'database' => 'cmidb',
-    'username' => 'root',
-    'password' => '',
-    'collation' => 'utf8_general_ci',
-    'prefix' => ''
-);
+$app->get('/', function(Request $request, Response $response){
+	$response->getBody()->write("Coucou");
+	return $response;
+});
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
 
-$container = new Illuminate\Container\Container;
-$connFactory = new \Illuminate\Database\Connectors\ConnectionFactory($container);
-$conn = $connFactory->make($settings);
-$resolver = new \Illuminate\Database\ConnectionResolver();
-$resolver->addConnection('default', $conn);
-$resolver->setDefaultConnection('default');
-\Illuminate\Database\Eloquent\Model::setConnectionResolver($resolver);
+    return $response;
+});
 
+$app->get('/contact', function(Request $request, Response $response){
+	$response->getBody()->write("Contact");
+	return $response;
+});
 $app->run();
-
-?>
