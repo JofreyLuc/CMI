fdb14a299b7ee10c0aed37e20e3d33db0e3a8976
mulhauser <mulhauser.remy@gmail.comr>
2016-05-17 12:55:50 +0200

    script BDD

--- projet/src/scriptPeuplerBDD.php
+++ projet/src/scriptPeuplerBDD.php
@@ -6,28 +6,26 @@
  * Time: 13:59
  */
 
-require 'app/model/Livre.php';
+//require 'app/model/Livre.php';
 
 
-$dir    = 'epub';
-// On scan le dossier epub pour récupérer chaque dossier d'epub
-$epub = scandir($dir);
-// Nombre de livre dans la base Gutemberg
-$nbLivre = count($epub) - 3;
-$livre;
-// On instancie un dom xml
-$dom = new DomDocument();
+		$dir = 'epub';
+		// On scan le dossier epub pour récupérer chaque dossier d'epub
+		$epub = scandir($dir);
 
-for($i = 1; $i < 2;$i++){
-    $chemin = 'epub/'.$i.'/pg'.$i.'.rdf';
-    $dom->load($chemin);
-    $livre = Livre::create();
-    $livre->title = $dom->documentElement->getElementsByTagName('title')->item(0)->nodeValue."\n";
-    $livre->auteur = $dom->documentElement->getElementsByTagName('name')->item(0)->nodeValue."\n";
-    $livre->langue = $dom->documentElement->getElementsByTagName('language')->item(0)->nodeValue."\n";
-    $livre->save();
+		// Nombre de livre dans la base Gutemberg
+		$nbLivre = count($epub) - 3;
 
-
-}
-
-
+		// On instancie un dom xml
+		$dom = new DOMDocument();
+		for($i = 1; $i < 2;$i++){
+		    $chemin = 'epub/'.$i.'/pg'.$i.'.rdf';
+		    $dom->load($chemin);
+		    
+		    $livre = new app\Model\Livre;
+		    $livre->titre = $dom->documentElement->getElementsByTagName('title')->item(0)->nodeValue."\n";
+		    $livre->auteur = $dom->documentElement->getElementsByTagName('name')->item(0)->nodeValue."\n";
+		    $livre->langue = $dom->documentElement->getElementsByTagName('language')->item(0)->nodeValue."\n";
+		    $livre->save();
+		    //echo $dom->documentElement->getElementsByTagName('title')->item(0)->nodeValue;
+		}
