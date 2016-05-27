8c74e624c39dfe273d4c4c2801753d9bf24fa4c0
wolkowicz <michelwolkowicz@yahoo.fr>
2016-05-27 10:55:51 +0200

    ajax opérationnel

--- projet/src/fac.js
+++ projet/src/fac.js
@@ -14,6 +14,7 @@ fac.modules.app = (function () {
 				data: data,
 				type: "POST",
 				success: callback,
+				async: false,
 				error: function (jqXHR, textStatus, errorThrown) {
 					console.log('URL : ' + url);
 					console.log(jqXHR);
@@ -27,6 +28,7 @@ fac.modules.app = (function () {
 				url: url,
 				type: "GET",
 				success: callback,
+				async: false,
 				done: function(response){return response;},
 				error: function (jqXHR, textStatus, errorThrown) {
 					console.log('url : ' + url);
@@ -45,104 +47,106 @@ fac.modules.app = (function () {
 
 fac.modules.users = (function(){
 	return {
-	init: function() {
-		$('#btn-users').click(function(){
-			fac.modules.app.get('http://localhost:8888/CMI/projet/src/users1', function(data) {
-				console.log(data);
+		init: function() {
+			$('#btn-users').click(function(){
+				fac.modules.app.get('http://localhost:8888/CMI/projet/src/users1', function(data) {
+					console.log(data);
+				});
 			});
-		});
 
-		$('#btn-create').click(function() {
-			var user = {
-				email: "Romain",
-				password: "dsfdgd",
-				nom: "dago",
-				prenom: "rom",
-				pseudo: "fdvdf"
-			};
-            // Si on veut savoir le type du json, à voir ???
-			// var json = JSON.stringify({User: user});
-			var json = JSON.stringify(user);
-			console.log(json);
+			$('#btn-create').click(function() {
+				var user = {
+					email: "Romain",
+					password: "dsfdgd",
+					nom: "dago",
+					prenom: "rom",
+					pseudo: "fdvdf"
+				};
+				// Si on veut savoir le type du json, à voir ???
+				// var json = JSON.stringify({User: user});
+				var json = JSON.stringify(user);
+				console.log(json);
 				fac.modules.app.post('http://localhost:8888/CMI/projet/src/user/new', user, function(data) {
 					console.log(data);
 				});
-		});
-
-
-
-/*
-		// script pour ajout d'un livre dans sa biblio perso
-		$('.importBiblioButton').click(function() {
-			// recupération du livre
-			var livre = fac.modules.app.get('/CMI/projet/src/api/books/1', function(data) {
-				console.log(data);
-			});
-			alert(livre);
-
-
-			//var idLivre = livre['idLivre'];
-			//console.log(idLivre);
-			//var data = {idLivre:"666", numeroPage:"2"};
-
-		//	var boutonid = document.getElementById('boutonAdd1').getAttribute('id');console.log(boutonid)
-
-			// Si on veut savoir le type du json, à voir ???
-			// var json = JSON.stringify({User: user});
-			//var json = JSON.stringify(user);
-			//console.log(json);
-
-			//fac.modules.app.post('/CMI/projet/src/api/users/1/library/web', data, function(data) {
-		//		console.log(data);
-			//});
-
-
-		});
-
-
-
-*/
-		$('.importBiblioButton').click(function() {
-			// recuperation de l'id
-			var urlLivre = $(this).attr('id');
-			// concatenation
-			var urlLivreEntier = '/CMI/projet/src/api/books/'+urlLivre;
-
-			var livre = $.ajax({
-				type: 'GET',
-				url: urlLivreEntier,
-				async: false,
-				//dataType: 'json',
-				//data: {action: 'getHotelsList'},
-				done: function (results) {
-					JSON.parse(results);
-					return results;
-				},
-				fail: function (jqXHR, textStatus, errorThrown) {
-					console.log('Could not get posts, server response: ' + textStatus + ': ' + errorThrown);
-				}
-			}).responseJSON;
-
-
-			//console.log(livre[0].idLivre);
-			var id = livre[0].idLivre;
-			// creation des data à envoyer
-			var biblio = {
-				idLivre: id,
-				positionLecture: 0
-			};
-
-
-
-			// envoie du resultat
-
-
-			// l'id de l'user est entré en dur
-			fac.modules.app.post('/CMI/projet/src/api/users/1/library/web', biblio, function(data) {
-				console.log(data);
 			});
 
-		});
+
+
+			/*
+			 // script pour ajout d'un livre dans sa biblio perso
+			 $('.importBiblioButton').click(function() {
+			 // recupération du livre
+			 var livre = fac.modules.app.get('/CMI/projet/src/api/books/1', function(data) {
+			 console.log(data);
+			 });
+			 alert(livre);
+
+
+			 //var idLivre = livre['idLivre'];
+			 //console.log(idLivre);
+			 //var data = {idLivre:"666", numeroPage:"2"};
+
+			 //	var boutonid = document.getElementById('boutonAdd1').getAttribute('id');console.log(boutonid)
+
+			 // Si on veut savoir le type du json, à voir ???
+			 // var json = JSON.stringify({User: user});
+			 //var json = JSON.stringify(user);
+			 //console.log(json);
+
+			 //fac.modules.app.post('/CMI/projet/src/api/users/1/library/web', data, function(data) {
+			 //		console.log(data);
+			 //});
+
+
+			 });
+
+
+
+			 */
+			$('.importBiblioButton').click(function() {
+				// recuperation de l'id
+				var idLivre = $(this).attr('id');
+				// concatenation
+				var urlLivreEntier = '/CMI/projet/src/api/books/'+idLivre;
+
+				/*var livre = $.ajax({
+					type: 'GET',
+					url: urlLivreEntier,
+					async: false,
+					//dataType: 'json',
+					//data: {action: 'getHotelsList'},
+					done: function (results) {
+						JSON.parse(results);
+						return results;
+					},
+					fail: function (jqXHR, textStatus, errorThrown) {
+						console.log('Could not get posts, server response: ' + textStatus + ': ' + errorThrown);
+					}
+				}).responseJSON;*/
+
+
+
+				//var id = livre[0].idLivre;
+				// creation des data à envoyer
+				var biblio = {
+					idLivre: idLivre,
+					positionLecture: 0
+				};
+
+
+
+				// envoie du resultat
+
+
+				// l'id de l'user est entré en dur
+				fac.modules.app.post('/CMI/projet/src/api/users/1/library/web', biblio, function(data) {
+					//console.log(data);
+				});
+				//$(this).value = "lalalala";
+				$(this).attr('value',returndata);
+
+			});
 
 
 
@@ -154,17 +158,10 @@ fac.modules.users = (function(){
 
 
 
-	}
+		}
 	}
 })();
 
 $(document).ready(function() {
 	fac.modules.users.init();
 });
-
-
-
-
-
-
-
