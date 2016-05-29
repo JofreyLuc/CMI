8f931b813bba1eba9dcaa1d6bc6e188deca3b98a
Alexis <ale.lanoix@gmail.com>
2016-05-29 12:01:05 +0200

    vérification middleware du token (mobile) ACTIVEE

--- projet/src/middleware/TokenAuth.php
+++ projet/src/middleware/TokenAuth.php
@@ -25,7 +25,6 @@ class TokenAuth extends \Slim\Middleware {
      * @return bool
      */
     public function authenticate($idUser, $token) {
-        return true;
         if (!isset($idUser) || !isset($token))
             return false;
         return \app\controller\UtilisateurController::validateToken($idUser, $token);
